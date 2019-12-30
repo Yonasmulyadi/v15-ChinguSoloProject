bd70fa9834a6d7ef5d47b6182f6eefc5a63aa6d4
Yonas <ymul0001@student.monash.edu>
2019-12-30 15:44:19 +0700

    add animation for Jumbotron

--- script.js
+++ script.js
@@ -2,12 +2,25 @@
  * Variables selection
  */
 const navigationSection = document.querySelector("#navigation-section");
+const jumbotronElement = [document.querySelector('#jumbotron__title'), document.querySelector('#jumbotron__note'), document.querySelector('#jumbotron .slack-link')];
+const jumbotronTitle = document.querySelector('#jumbotron__title')
 
 
+// .features__card
 
 /**
  * Website's functionality
  */
+
+window.onload = () => {
+    for (let i = 0; i < jumbotronElement.length; i++)
+    {
+        jumbotronElement[i].style.opacity = 1;
+        jumbotronElement[i].style.transform = "translateY(0px)";
+    }
+};
+
+
 document.querySelector("#navigation-button").addEventListener('click',() => {    
     navigationSection.style.height == '0vh' ? navigationSection.style.height = '30vh' : navigationSection.style.height = '0vh';
 })
